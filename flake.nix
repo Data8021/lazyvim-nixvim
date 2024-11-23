a03cf06e09e977604f3f6732182fe13b079ef0b2
Data8021 <bwales@gmail.com>
2024-11-23 17:53:19 -0500

    Add lsp back...

--- flake.nix
+++ flake.nix
@@ -153,9 +153,6 @@
                                         opts = function(_, opts) opts.ensure_installed = {} end
                                         vim.filetype.add({
                                           extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
-                                          filename = {
-                                            ["vifmrc"] = "vim",
-                                          },
                                           pattern = {
                                             [".*/waybar/config"] = "jsonc",
                                             [".*/mako/config"] = "ini",
@@ -182,6 +179,18 @@
                                       },
                                     },
                                   })
+
+                                vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+                                    pattern = {"*.hl", "hypr*.conf"},
+                                    callback = function(event)
+                                        print(string.format("starting hyprls for %s", vim.inspect(event)))
+                                        vim.lsp.start {
+                                            name = "hyprlang",
+                                            cmd = {"hyprls"},
+                                            root_dir = vim.fn.getcwd(),
+                                        }
+                                    end
+                                })
 
                                 local function map(mode, lhs, rhs, opts)
                 	                local options = { noremap = true, silent = true }
